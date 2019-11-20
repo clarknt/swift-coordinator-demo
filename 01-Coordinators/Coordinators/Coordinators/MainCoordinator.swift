f074f92d10e5e2241b577c19c08841fab73c9948
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-19 19:01:37 -0500

    Coordinators

---
+++ 01-Coordinators/Coordinators/Coordinators/MainCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  MainCoordinator.swift
+//  Coordinators
+//
+//  Created by clarknt on 2019-11-19.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    func buySubscription() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# swift-coordinators
-Template implementation of Swift coordinators using Paul Hudson's tutorial
+# Swift Coordinators
+
+Template implementation of [Soroush Khanlou](http://khanlou.com)'s Coordinator design pattern using Paul Hudson's tutorials:
+- [How to use the coordinator pattern in iOS apps](https://www.hackingwithswift.com/articles/71/how-to-use-the-coordinator-pattern-in-ios-apps)
+- [Advanced coordinators in iOS](https://www.hackingwithswift.com/articles/175/advanced-coordinator-pattern-tutorial-ios)
+
+Requirements: iOS 13 and XCode 11.
