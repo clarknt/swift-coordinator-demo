50fd66b352c68d248c22570e2e0d6f12a99efa64
clarknt <24315769+clarknt@users.noreply.github.com>
2019-11-19 21:05:28 -0500

    Navigating backwards

---
+++ 03-NavigatingBackwards/NavigatingBackwards/Coordinators/MainCoordinator.swift
@@ -0,0 +1,67 @@
+//
+//  MainCoordinator.swift
+//  Coordinators
+//
+//  Created by clarknt on 2019-11-19.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
+    var NavigatingBackwards = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        navigationController.delegate = self
+
+        let vc = ViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+
+    func buySubscription() {
+        let child = BuyCoordinator(navigationController: navigationController)
+        child.parentCoordinator = self
+        NavigatingBackwards.append(child)
+        child.start()
+    }
+
+    func createAccount() {
+        let vc = CreateAccountViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in NavigatingBackwards.enumerated() {
+            if coordinator === child {
+                NavigatingBackwards.remove(at: index)
+                break
+            }
+        }
+    }
+
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        // Read the view controller we’re moving from.
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+
+        // Check whether our view controller array already contains that view controller. If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+        if navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+
+        // We’re still here – it means we’re popping the view controller, so we can check whether it’s a buy view controller
+        if let buyViewController = fromViewController as? BuyViewController {
+            // We're popping a buy view controller; end its coordinator
+            childDidFinish(buyViewController.coordinator)
+        }
+    }
+}
---
+++ 03-NavigatingBackwards/NavigatingBackwards/ViewControllers/CreateAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountViewController.swift
+//  Coordinators
+//
+//  Created by clarknt on 2019-11-19.
+//  Copyright © 2019 clarknt. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController, Storyboarded {
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
