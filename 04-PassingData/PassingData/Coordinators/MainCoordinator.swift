b2b1bf87680fee8892036948d57d0681c080d059
clarknt <24315769+clarknt@users.noreply.github.com>
2019-12-03 13:55:42 -0500

    Correct name

--- 04-PassingData/PassingData/Coordinators/MainCoordinator.swift
+++ 04-PassingData/PassingData/Coordinators/MainCoordinator.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 class MainCoordinator: Coordinator {
-    var PassingData = [Coordinator]()
+    var childCoordinators = [Coordinator]()
     var navigationController: UINavigationController
 
     init(navigationController: UINavigationController) {
